2d77c92e105f0d58597d395ebb30e22667258258
Endryus Schmidel <endryusschmidel@gmail.com>
2024-11-04 01:44:44 -0300

    .

--- exercicios/tabuada/script.js
+++ exercicios/tabuada/script.js
@@ -2,5 +2,20 @@ function calcular() {
     var res = window.document.getElementById('res')
     var txtValor = document.getElementById('idinputValor')
     var numValor = Number(txtValor.value)
-    res.innerHTML = `valor é ${numValor}`
+    res.innerHTML = ` `
+    tot = 0
+    if (numValor != 0) {
+        res.innerHTML = `<hr>`
+        for (let i = 1; i <= 10; i++) {
+            tot += 1
+            let resValor = numValor * i
+            res.innerHTML += ` ${numValor} x ${i} = ${resValor} <br> `
+            // if (tot == 5) {
+            //     res.innerHTML += `<br>`
+            // }
+        }
+    res.innerHTML += `<hr>`
+    } else {
+        window.alert('Insira um valor')
+    }
 }
